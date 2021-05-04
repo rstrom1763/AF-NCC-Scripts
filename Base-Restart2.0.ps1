a31527a416f231fbcc187f24e423eacb4ce681de
STROM <1555621155@mil>
2021-05-04 10:55:22 -0500

    Create Base-Restart2.0.ps1

---
+++ Base-Restart2.0.ps1
@@ -0,0 +1,45 @@
+﻿function Restart-Base {
+
+    param(
+
+        [Parameter(Mandatory = $True)]$computers,
+        [int]$delay,
+        $restartDelay,
+        $message
+
+    )
+    if ($null -ne $delay) {
+        
+        Start-Sleep -Seconds $delay
+
+    }
+    $computers = Get-Content $computers
+    $computers = $computers | Invoke-Ping -quiet
+
+    $scriptblock = {
+
+        param(
+            $message,
+            $restartDelay
+        )
+
+        Function Message-Popup {
+
+            Param([Parameter(Mandatory = $True)][String]$Message)
+
+            Invoke-WmiMethod -Class Win32_Process -Name Create -ArgumentList "C:\Windows\System32\msg.exe * $message"
+
+        }
+
+        Message-Popup -Message $message 
+        Start-Sleep -Seconds $delay
+        Restart-Computer -Force
+
+    }
+
+    foreach ($PC in $computers) {
+
+        Invoke-Command -ComputerName $PC $scriptblock -ArgumentList $message, $restartDelay -AsJob
+
+    }
+}
