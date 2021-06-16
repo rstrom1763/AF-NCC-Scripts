292c7eb929729a501774e589875f3b4fb810eafb
STROM <1555621155@mil>
2021-06-16 14:34:29 -0500

    Update Send-Job.ps1
    Added collection of serial number and mac address to the scriptblock.

--- Computer_Reports/Send-Job.ps1
+++ Computer_Reports/Send-Job.ps1
@@ -397,14 +397,19 @@ Function Send-Job {
         $model = $model.Trim()
         Add-Member -InputObject $data -Name "Model" -Value $model -MemberType NoteProperty
 
+        $serial = (Get-WmiObject win32_bios).Serialnumber
+        Add-Member -InputObject $data -Name "Serial Number" -Value $serial -MemberType NoteProperty
+
         $ip = Test-Connection -ComputerName (hostname) -Count 1  | Select-Object IPV4Address
         Add-Member -InputObject $data -Name "IP" -Value $ip.IPV4Address.ToString() -MemberType NoteProperty
+
+        $mac = (Get-NetAdapter | Where-Object {$_.Status -eq "Up"}).MacAddress
+        Add-Member -InputObject $data -Name "MAC" -Value $mac -MemberType NoteProperty
 
         $bios = Get-BiosType
         if ($bios -eq 1) { $bios = "Legacy BIOS" }
         elseif ($bios -eq 2) { $bios = "UEFI" }
         else { $bios = "Other" }
-
         Add-Member -InputObject $data -Name "BIOS" -Value $bios -MemberType NoteProperty
 
         $secureBoot = Confirm-SecureBootUEFI
