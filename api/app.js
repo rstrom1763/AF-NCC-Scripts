e7148f41b7410796031189c5a202b56475f1a2a4
Ryan <48137622+rstrom1763@users.noreply.github.com>
2022-07-15 22:51:15 -0500

    Added JSON download button
    There is now a button that downloads the results as a json file.

--- api/app.js
+++ api/app.js
@@ -20,6 +20,7 @@ app.post('/write', (req, res) => {
     } else {
         computername = req.body.ComputerName;
     }
+    computername = computername.toLowerCase()
     console.log(computername);
     fs.writeFile(datadir + computername + '.json', JSON.stringify(req.body), (err) => err);
     res.send("Update Written");
@@ -44,4 +45,18 @@ app.get('/getcomputer/:id', (req, res) => {
 
 app.get('/', (req, res) => {
     res.send(fs.readFileSync('index.html', 'utf8'))
+});
+
+app.get('/downloadjson/:computername', function (req, res) {
+    var computername = req.params['computername'].toLowerCase()
+    const file = datadir + computername + '.json'
+    console.log(file)
+    try {
+        res.download(file); // Set disposition and send it.
+    } catch (e) {
+        res.setHeader('Content-Type', 'text/plain')
+        res.statusCode = 404
+        res.send("Not found")
+        return
+    }
 });
--- api/form.js
+++ api/form.js
@@ -1,16 +1,14 @@
 function printJsonResult(data) {
-
     //Reset div
     var div = document.getElementById("main");
-    while (div.firstChild) {
+    while (div.firstChild) { //Reset the div
         div.removeChild(div.firstChild);
     }
-
     //Set div with JSON information
     data = JSON.parse(data)
     for (key in data) {
         var div = document.createElement("div");
-        div.className = "center"
+        div.className = "results"
         div.style.color = "black"
         div.style.width = "25%"
         div.style.padding = "24px"
@@ -20,19 +18,56 @@ function printJsonResult(data) {
     }
 }
 
+
+function download_json(computername) {
+    var request = new XMLHttpRequest();
+    request.responseType = "blob";
+    request.onreadystatechange = function () {
+        if (this.readyState == 4 && request.status === 200){
+            const file = window.URL.createObjectURL(this.response);
+            const anchor = document.createElement("a");
+            anchor.href = file;
+            anchor.download = computername + ".json";
+            document.body.appendChild(anchor);
+            anchor.click();
+        }
+        if (this.readyState == 4 && request.status === 404) {
+            return
+        }
+    }
+    request.open("GET", 'http://' + location.host + '/downloadjson/' + computername, true);
+    request.send();
+}
+
+
+function create_download_button() {
+    div = document.getElementById("download")
+    while (div.firstChild) { //Reset the div
+        div.removeChild(div.firstChild);
+    };
+    var button = document.createElement("button");
+    button.onclick = function () {
+        download_json(document.getElementById("input").value)
+    }
+    button.innerHTML = "Download as JSON";
+    document.getElementById("download").appendChild(button)
+};
+
+
 function send_data(data) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === xhr.DONE) {
-            if (xhr.status === 200) {
-                printJsonResult(xhr.response)
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === request.DONE) {
+            if (request.status === 200) {
+                create_download_button();
+                printJsonResult(request.response);
             }
-            if (xhr.status === 404) {
+            if (request.status === 404) {
                 document.getElementById('output').innerHTML = '404 not found'
             }
         }
     }
-    xhr.open("GET", 'http://' + location.host + '/getcomputer/' + data, true);
-    xhr.send(data.toLowerCase());
-
+    request.open("GET", 'http://' + location.host + '/getcomputer/' + data, true);
+    request.send(data.toLowerCase());
 };
+
